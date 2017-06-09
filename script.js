73bc4ab812b94e35a3aa64832eb1023554cf0f03
AsiaKo <aasiakozik@gmail.com>
2017-06-08 20:29:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//smooth scrolling 
+
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault(); 
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1300, function(){
+        window.location.hash = hash;
+      });
+    
+  };
+});
+  
+
+//back to top 
+
+jQuery(document).ready(function() {
+        var offset = 250;
+        var duration = 300;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.back-to-top').fadeIn(duration);
+            } else {
+                jQuery('.back-to-top').fadeOut(duration);
+            }
+        });
+        jQuery('.back-to-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 1500}, duration);
+            return false;
+        });
+    });
+
+})
+ 
